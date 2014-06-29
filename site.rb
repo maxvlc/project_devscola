8b6d099078d1c4fa0c1ce3138dcc9c6bf507c28f
maxvlc <maxcar@gmail.com>
2014-06-29 18:17:58 +0200

    Basic page functionality, making invitation cards and generating URL

--- site.rb
+++ site.rb
@@ -6,5 +6,18 @@ get '/' do
 end
 
 post '/form' do
-	erb :selection, :locals => {:user_data => params}
+	erb :selection, :locals => {:party_data => params}
+end
+
+post '/cards' do
+	erb :generator, :locals => {:party_data => params}
+end
+
+get '/:card/:whoinvites/:guestname/:eventdate/:event/:eventplace' do
+  erb :final_card, :locals => { :party_data => params}
+end
+
+not_found do
+  status 404
+  'not found'
 end
