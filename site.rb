8c9dd02b09e78adb4e09193a6c482724e92ef5b1
maxvlc <maxcar@gmail.com>
2014-06-30 19:33:50 +0200

    CSS, 404 error, Preview Template

--- site.rb
+++ site.rb
@@ -6,18 +6,18 @@ get '/' do
 end
 
 post '/form' do
-	erb :selection, :locals => {:party_data => params}
+	erb :selection, :locals => { :party_data => params }
 end
 
 post '/cards' do
-	erb :generator, :locals => {:party_data => params}
+	erb :generator, :locals => { :party_data => params }
 end
 
 get '/:card/:whoinvites/:guestname/:eventdate/:event/:eventplace' do
-  erb :final_card, :locals => { :party_data => params}
+  erb :final_card, :locals => { :party_data => params }
 end
 
 not_found do
   status 404
-  'not found'
+  erb :oops, :layout => false
 end
